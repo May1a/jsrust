a4b154c8a23074dfa25c3b0d2b50115ca1924a80
May1a <may@localhost>
2026-02-18 19:22:42 +0100

    something closer to something desirable

--- tokenizer.js
+++ tokenizer.js
@@ -13,7 +13,7 @@ const TokenType = {
     Fn: iota(),
     Let: iota(),
     Const: iota(),
-    Bool: iota(),
+    Static: iota(),
     True: iota(),
     False: iota(),
     Type: iota(),
@@ -24,6 +24,12 @@ const TokenType = {
     Unsafe: iota(),
     If: iota(),
     Match: iota(),
+    OpenParen: iota(),
+    CloseParen: iota(),
+    OpenCurly: iota(),
+    CloseCurly: iota(),
+    OpenSquare: iota(),
+    CloseSquare: iota(),
 };
 
 /**
@@ -31,11 +37,27 @@ const TokenType = {
  */
 function toToken(str) {
     switch (str) {
-        case "fn":
-            return TokenType.Fn;
-        case "let":
-            return TokenType.Let;
-        case "const":
-            return 
+        case "fn": return TokenType.Fn;
+        case "let": return TokenType.Let;
+        case "const": return TokenType.Const;
+        case "static": return TokenType.Static;
+        case "true": return TokenType.True;
+        case "false": return TokenType.False;
+        case "type": return TokenType.Type;
+        case "use": return TokenType.Use;
+        case "pub": return TokenType.Pub;
+        case "enum": return TokenType.Enum;
+        case "struct": return TokenType.Struct;
+        case "unsafe": return TokenType.Unsafe;
+        case "if": return TokenType.If;
+        case "match": return TokenType.match;
+        case "(": return TokenType.OpenParen;
+        case ")": return TokenType.CloseParen;
+        case "[": return TokenType.OpenSquare;
+        case "]": return TokenType.CloseSquare;
+        case "{": return TokenType.OpenCurly;
+        case "}": return TokenType.CloseCurly;
+        default: break;
     }
+    return TokenType.Invalid;
 }
